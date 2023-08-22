f97e3820192e874ae0fa81d83c11ee5586b570ed
YunHyebin <103030744+YunHyebin@users.noreply.github.com>
2023-08-22 10:19:40 +0900

    fix: 번역 api 요청 컨트롤러 수정

--- src/main/java/com/example/WorldChatProject/randomChat/controller/RandomTranslateController.java
+++ src/main/java/com/example/WorldChatProject/randomChat/controller/RandomTranslateController.java
@@ -1,13 +1,13 @@
 package com.example.WorldChatProject.randomChat.controller;
 
-import com.example.WorldChatProject.randomChat.dto.RandomChatDTO;
+import com.example.WorldChatProject.randomChat.dto.RandomTranslateDTO;
 import com.example.WorldChatProject.randomChat.service.RandomTranslateService;
+import com.example.WorldChatProject.user.dto.UserDTO;
+import com.example.WorldChatProject.user.security.auth.PrincipalDetails;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 
 @RestController
 @RequestMapping("/randomTranslate")
@@ -16,14 +16,20 @@ import java.net.URLEncoder;
 public class RandomTranslateController {
     private final RandomTranslateService translateService;
 
-    @PostMapping("")
-    public void translate(@RequestBody RandomChatDTO randomChatDTO) {
-        String text = randomChatDTO.getContent();
-        translateService.translate(text);
+    @PostMapping("/translate")
+    public String translate(@RequestBody RandomTranslateDTO randomTranslateDTO, Authentication authentication) {
+        PrincipalDetails principal = (PrincipalDetails) authentication.getPrincipal();
+        UserDTO userDTO = principal.getUser();
+        String nation = userDTO.getUserNationality();
+        String sourceCode = randomTranslateDTO.toSourceCode(nation);
+        randomTranslateDTO.setSourceCode(sourceCode);
+        return translateService.translate(randomTranslateDTO);
+    }
 
-
-
+    @PostMapping("detect")
+    public String detect(@RequestBody RandomTranslateDTO randomTranslateDTO) {
 
     }
+
 
 }
